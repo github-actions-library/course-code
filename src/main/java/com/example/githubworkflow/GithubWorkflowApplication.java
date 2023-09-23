c07adc9ad6834e55df95154803890bc590a1b9b4
srajeshv <shashankvalapa@gmail.com>
2023-09-23 15:26:10 +0530

    first commit

---
+++ src/main/java/com/example/githubworkflow/GithubWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubworkflow/GithubWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
